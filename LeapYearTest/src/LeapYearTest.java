a5eefaa399df70bf2f5c85f70f970c31e0e21ceb
ATIQUR RAHMAN ALINE <aline.atik@yahoo.com>
2018-01-25 10:46:21 +0600

    new commit

---
+++ LeapYearTest/src/LeapYearTest.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LeapYearTest {
+    public static void main(String[] args) {
+
+        System.out.print("Enter year : ");
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+
+        if (year % 4 == 0){
+            System.out.print("It is a leapyear!");
+        }else if(year % 100 == 0){
+            System.out.print("It is a leapyear!");
+        }else {
+            System.out.print("Not leap year!");
+        }
+    }
+}
