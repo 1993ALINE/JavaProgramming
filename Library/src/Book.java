1d7bd474294f927ab4801158901cf70bdd345348
ATIQUR RAHMAN ALINE <aline.atik@yahoo.com>
2018-01-28 18:34:13 +0600

    new commit

---
+++ Library/src/Book.java
@@ -0,0 +1,7 @@
+public class Book extends Library {
+    public static void main(String[] args) {
+        Library b = new Book();
+        b.book_id();
+        b.book_issue_date();
+    }
+}
---
+++ Library/src/Library.java
@@ -0,0 +1,9 @@
+public class Library {
+    void book_id(){
+        System.out.println("Every book inherits a book id from library");
+    }
+    void book_issue_date(){
+        System.out.println("Every book has a book issue date from Librarian");
+
+    }
+}
